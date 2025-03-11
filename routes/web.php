fcd8782d146e96fd3bfcaa97d4f94dd086bcdd8e
Leandro Gonçalves <lleandrogon2004@gmail.com>
2025-03-11 20:54:08 -0300

    Primeiro commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use Illuminate\Support\Facades\Route;
+
+/* Route::get('/', function () {
+    return 'Olá, seja bem vindo ao curso!';
+}); */
+
+Route::get('/', [PrincipalController::class, 'principal']);
+
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
+
+Route::get('/contato', [ContatoController::class, 'contato']);
+
+/* verbo http
+
+get
+post
+put
+patch
+delete
+options
+
+*/
