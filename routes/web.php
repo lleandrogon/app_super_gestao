db46aa1fa4ef738bff28b086874fb899ac4f58f5
Leandro Gonçalves <lleandrogon2004@gmail.com>
2025-03-12 21:10:32 -0300

    Rotas

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,32 @@ use Illuminate\Support\Facades\Route;
     return 'Olá, seja bem vindo ao curso!';
 }); */
 
-Route::get('/', [PrincipalController::class, 'principal']);
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function() { return 'Login'; })->name('site.login');
 
-Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function() { return 'Clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', function() { return 'Fornecedores'; })->name('app.fornecedores');
+    Route::get('/produtos', function() { return 'Produtos'; })->name('app.produtos');
+});
 
-Route::get('/contato', [ContatoController::class, 'contato']);
+Route::get('/rota1', function() {
+    echo "Rota 1";
+})->name('site.rota1');
+
+Route::get('/rota2', function() {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+
+//Route::redirect('rota2', 'rota1');
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe, <a href="'.route('site.index').'">clique aqui</a> para ir para página inicial';
+});
+
 
 /* verbo http
 
