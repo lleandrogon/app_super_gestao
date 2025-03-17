2266e8d9e6327eac5e15bf41779e416ea5c05443
Leandro Gonçalves <lleandrogon2004@gmail.com>
2025-03-17 01:01:29 -0300

    Adicionando CSS ao blade

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -16,15 +16,35 @@
 
 @isset ($fornecedores)
 
-    @foreach ($fornecedores as $indice => $fornecedor)
-         Fornecedor: {{ $fornecedor['nome'] }}
+    @forelse ($fornecedores as $indice => $fornecedor)
+
+        @dd($loop)
+
+        Iteração atual: {{ $loop->iteration }}
+        <br>
+        Fornecedor: {{ $fornecedor['nome'] }}
         <br>
         Status: {{ $fornecedor['status'] }}
         <br>
         CNPJ: {{ $fornecedor['cnpj'] ?? 'Dado não foi preenchido' }}
         <br>
         Telefone: ({{ $fornecedor['ddd'] ?? '' }}) ({{ $fornecedor['telefone'] ?? '' }})
+        <br>
+        @if ($loop->first)
+            Primeira iteração do Loop
+
+            <br>
+            Total de registros: {{ $loop->count }}
+        @endif
+
+        @if ($loop->last)
+            Última iteração do Loop
+
+            
+        @endif
         <hr>
-    @endforeach
+    @empty
+        Não existe fornecedores cadastrados!!!
+    @endforelse
 
 @endisset
