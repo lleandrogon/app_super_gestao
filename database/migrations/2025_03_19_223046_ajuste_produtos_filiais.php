a123a42d5e0fe2ad98077d68c6f98bfaf716d0ba
Leandro Gonçalves <lleandrogon2004@gmail.com>
2025-03-19 21:33:33 -0300

    Relação de muitos pra muitos

---
+++ database/migrations/2025_03_19_223046_ajuste_produtos_filiais.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //Criando a tabela filiais
+        Schema::create('filiais', function(Blueprint $table) {
+            $table->id();
+            $table->string('filial', 30);
+            $table->timestamps();
+        });
+
+        //Criando a tabela filiais
+        Schema::create('produto_filiais', function(Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('filial_id');
+            $table->unsignedBigInteger('produto_id');
+            $table->decimal('preco_venda', 8, 2);
+            $table->integer('estoque_minimo');
+            $table->integer('estoque_maximo');
+            $table->timestamps();
+
+            //foreign key (constraints)
+            $table->foreign('filial_id')->references('id')->on('filiais');
+            $table->foreign('produto_id')->references('id')->on('produtos');
+        });
+
+        //removendo colunas da tabela produtos
+        Schema::table('produtos', function(Blueprint $table) {
+            $table->dropColumn(['preco_venda', 'estoque_minimo', 'estoque_maximo']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //adicionar colunas da tabela produtos
+        Schema::table('produtos', function(Blueprint $table) {
+            $table->decimal('preco_venda', 8, 2);
+            $table->integer('estoque_minimo');
+            $table->integer('estoque_maximo');
+        });
+
+        Schema::dropIfExists('produtos_filiais');
+        
+        Schema::dropIfExists('filiais');
+    }
+};
