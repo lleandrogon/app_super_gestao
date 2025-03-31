aa91c302f3d5795bda5021cf1b804b43b4ecaa3d
Leandro Gonçalves <lleandrogon2004@gmail.com>
2025-03-31 20:19:13 -0300

    Funcionalidades no formulário e tabela MotivoContato

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -4,38 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\SiteContato;
+use App\Models\MotivoContato;
 
 class ContatoController extends Controller
 {
     public function contato(Request $request) {
 
-        /*
-        echo '<pre>';
-        print_r($request->all());
-        echo '</pre>';
-        echo $request->input('nome');
-        echo '<br>';
-        echo $request->input('email');
-        */
+        $motivo_contatos = MotivoContato::all();
 
-        /*
-        $contato = new SiteContato();
-        $contato->nome = $request->input('nome');
-        $contato->telefone = $request->input('telefone');
-        $contato->email = $request->input('email');
-        $contato->motivo_contato = $request->input('motivo_contato');
-        $contato->mensagem = $request->input('mensagem');
+        return view('site.contato', ['titulo' => 'Contato', 'motivo_contatos' => $motivo_contatos]);
+    }
 
-        // print_r($contato->getAttributes());
-        $contato->save();
-        */
+    public function salvar(Request $request) {
 
-        $contato = new SiteContato();
-        $contato->create($request->all());
-        //$contato->save();
-        //print_r($contato->getAttributes());
+        //realizar a validação dos dados do formulários recebidos no request
 
-        return view('site.contato', ['titulo' => 'Contato']);
+        $request->validate([
+            'nome' => 'required|min:3|max:40',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required|max:2000'
+        ]);
+        
+
+       // SiteContato::create($request->all());
     }
 }
--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\MotivoContato;
 
 class PrincipalController extends Controller
 {
     public function principal() {
-        return view('site.principal');
+
+        $motivo_contatos = MotivoContato::all();
+
+        return view('site.principal', ['motivo_contatos' => $motivo_contatos]);
     }
 }
